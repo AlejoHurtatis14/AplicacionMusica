095920853ec24ab504f05a3f5ae3c62a66c3a8bd
AlejoHurtatis14 <neideralejandro.h@gmail.com>
2020-07-12 18:12:29 -0500

    Se organiza error con cors.

--- src/app/components/home/home-component/home.component.ts
+++ src/app/components/home/home-component/home.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase';
 import { LoginService } from 'src/app/shared/services/login.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -16,7 +17,8 @@ export class HomeComponent implements OnInit {
   constructor(
     private dataBase: AngularFirestore,
     private fireAuth: AngularFireAuth,
-    private loginService: LoginService
+    private loginService: LoginService,
+    private http: HttpClient
   ) {
     this.fireAuth.authState.subscribe(user => {
       console.log("User ", user);
@@ -29,7 +31,22 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void { }
 
   executeLogin(type) {
-    this.loginService.loginUser(type);
+    const url = environment.complementoMusixMatch + 'track.search?q_artist=justin bieber&page_size=3&page=1&s_track_rating=desc&apikey=24cfbf9043e212897e8f2927daecb7f0';
+    const google = environment.complementoApiGoogle + 'oauth?client_id=719003813794-s9932lepg5rl10q1t3ouevb5doiaublb.apps.googleusercontent.com&redirect_uri=http://localhost:4200&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email';
+    // const httpOptions = {
+    //   headers: new HttpHeaders({
+    //     'Access-Control-Origin': '*',
+    //     'Access-Control-Allow-Origin': 'http://127.0.0.1:4200/',
+    //     'Accept': 'text/plain',
+    //   })
+    // };
+    // track.search?q_artist=justin bieber&page_size=3&page=1&s_track_rating=desc&apikey=24cfbf9043e212897e8f2927daecb7f0
+    this.http.get(url).subscribe(respuesta => {
+      console.log("respuesta ", respuesta);
+    }, error => {
+      console.log("Error ", error);
+    });
+    // this.loginService.loginUser(type);
   }
 
   logOut() {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,8 +12,11 @@ export const environment = {
     storageBucket: "aplicacionmusica.appspot.com",
     messagingSenderId: "719003813794",
     appId: "1:719003813794:web:16abd717615fb181f154fb",
-    measurementId: "G-JGR5HV6SW8"
-  }
+    measurementId: "G-JGR5HV6SW8",
+  },
+  apiKeyMusiMatch: "24cfbf9043e212897e8f2927daecb7f0",
+  complementoMusixMatch: "/ws/1.1/",
+  complementoApiGoogle: "/signin/",
 };
 
 /*
@@ -26,4 +29,4 @@ export const environment = {
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
 
 //URL peticion de acceso a plataforma con api de google
-//https://accounts.google.com/o/oauth2/v2/auth?client_id=719003813794-uvajjr2mg6q55ml0jakouko0hj78s4hn.apps.googleusercontent.com&redirect_uri=https://localhost:4200/&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email
+//https://accounts.google.com/o/oauth2/v2/auth?client_id=719003813794-s9932lepg5rl10q1t3ouevb5doiaublb.apps.googleusercontent.com&redirect_uri=http://localhost:4200&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email
